b9fa47b2f955bddd8c9199f4a78d22987bcca5c5
aparvate <amogh.parvate@gmail.com>
2023-02-24 03:37:04 -0600

    Piglin brutes have armor now, and are scavs!

--- src/main/java/net/fabricmc/infernal_war/mixin/PiglinBruteEntityMixin.java
+++ src/main/java/net/fabricmc/infernal_war/mixin/PiglinBruteEntityMixin.java
@@ -1,22 +1,28 @@
 package net.fabricmc.infernal_war.mixin;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.fabricmc.infernal_war.common.item.RegisterItems;
 import net.minecraft.entity.CrossbowUser;
+import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.InventoryOwner;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.AbstractPiglinEntity;
 import net.minecraft.entity.mob.PiglinBruteEntity;
 import net.minecraft.entity.mob.PiglinEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
@@ -40,6 +46,38 @@ public class PiglinBruteEntityMixin {
         }
         else if (randVal <= 0.3){
             ((PiglinBruteEntity)(Object)this).equipStack(EquipmentSlot.MAINHAND, new ItemStack(RegisterItems.PIG_IRON_SCIMITAR));
+        }
+    }
+
+    @Inject(method = "initialize", at = @At("TAIL"))
+    public void initializeArmor(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt, CallbackInfoReturnable info) {
+        this.initArmor(world.getRandom(), difficulty);
+    }
+
+    protected void initArmor(Random random, LocalDifficulty localDifficulty) {
+        if ((double)random.nextFloat() <= 0.5){
+            this.equipAtChance(EquipmentSlot.HEAD, new ItemStack(RegisterItems.PIG_IRON_HELMET), random);
+        }
+        else{
+            this.equipAtChance(EquipmentSlot.HEAD, new ItemStack(Items.GOLDEN_HELMET), random);
+        }
+        if ((double)random.nextFloat() <= 0.5){
+            this.equipAtChance(EquipmentSlot.CHEST, new ItemStack(RegisterItems.PIG_IRON_CHESTPLATE), random);
+        }
+        else{
+            this.equipAtChance(EquipmentSlot.CHEST, new ItemStack(Items.GOLDEN_CHESTPLATE), random);
+        }
+        if ((double)random.nextFloat() <= 0.5){
+            this.equipAtChance(EquipmentSlot.LEGS, new ItemStack(RegisterItems.PIG_IRON_LEGGINGS), random);
+        }
+        else{
+            this.equipAtChance(EquipmentSlot.LEGS, new ItemStack(Items.GOLDEN_LEGGINGS), random);
+        }
+        if ((double)random.nextFloat() <= 0.5){
+            this.equipAtChance(EquipmentSlot.FEET, new ItemStack(RegisterItems.PIG_IRON_BOOTS), random);
+        }
+        else{
+            this.equipAtChance(EquipmentSlot.FEET, new ItemStack(Items.GOLDEN_BOOTS), random);
         }
     }
     
